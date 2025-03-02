bb83051979042eb69b534cec42679b0bad714898
SibelJr11 <107586362+SibelJr11@users.noreply.github.com>
2025-03-02 11:04:45 -0500

    Eliminando node_modules del repositorio

--- models/fincaModel.js
+++ models/fincaModel.js
@@ -71,33 +71,33 @@ exports.getByIdAdministrador = (conn, id) => {
 //Obtiene el total de kilos y de pagos a trabajadores de una finca en especifica y los agrupa por mes.
 exports.getByIdAndYear = (conn, idFinca, year) => {
       const sql = `SELECT 
-    DATE_FORMAT(p.fecha, '%M') AS mes,
-    COALESCE((
-        SELECT SUM(pagos.valor)
-        FROM pagos
-        WHERE pagos.id_asignacion = a.id_asignacion
-        AND YEAR(pagos.fecha) = ?
-    ), 0) AS total_pagos,
-    SUM(j.kilos) AS total_kilos
-FROM 
-    fincas f
-JOIN 
-    semanas s ON f.id_finca = s.id_finca
-JOIN 
-    asignaciones a ON s.id_semana = a.id_semana
-LEFT JOIN 
-    jornales j ON a.id_asignacion = j.id_asignacion
-LEFT JOIN 
-    pagos p ON a.id_asignacion = p.id_asignacion
-WHERE 
-    f.id_finca = ?
-AND 
-    YEAR(j.fecha) = ?
-GROUP BY 
-    mes
-ORDER BY 
-    MONTH(p.fecha);
-`;
+             DATE_FORMAT(p.fecha, '%M') AS mes,
+             COALESCE((
+                 SELECT SUM(pagos.valor)
+                 FROM pagos
+                 WHERE pagos.id_asignacion = a.id_asignacion
+                 AND YEAR(pagos.fecha) = ?
+             ), 0) AS total_pagos,
+             SUM(j.kilos) AS total_kilos
+         FROM 
+             fincas f
+         JOIN 
+             semanas s ON f.id_finca = s.id_finca
+         JOIN 
+             asignaciones a ON s.id_semana = a.id_semana
+         LEFT JOIN 
+             jornales j ON a.id_asignacion = j.id_asignacion
+         LEFT JOIN 
+             pagos p ON a.id_asignacion = p.id_asignacion
+         WHERE 
+             f.id_finca = ?
+         AND 
+             YEAR(j.fecha) = ?
+         GROUP BY 
+             mes
+         ORDER BY 
+             MONTH(p.fecha);
+         `;
       return new Promise((resolve, reject) => {
             conn.query(sql, [year, idFinca, year], (err, result) => {
                   if (err) return reject(err);
@@ -108,31 +108,36 @@ ORDER BY
 
 exports.getTodasFincasByIdAndYear = (conn, no_identificacion, year) => {
       const sql = `SELECT 
-               f.nombre,
-               CONVERT(COALESCE(SUM(p.valor), 0), SIGNED) AS total_pagos,
-               SUM(j.kilos) AS total_kilos
-           FROM 
-               fincas f
-           JOIN 
-               usuarios u ON f.id_propietario = u.no_identificacion
-           JOIN 
-               semanas s ON f.id_finca = s.id_finca
-           JOIN 
-               asignaciones a ON s.id_semana = a.id_semana
-           JOIN 
-               jornales j ON a.id_asignacion = j.id_asignacion
-           JOIN 
-               pagos p ON a.id_asignacion = p.id_asignacion
-           WHERE 
-               u.no_identificacion = ?
-           AND 
-              YEAR(p.fecha) = ?
-           GROUP BY 
-              f.nombre
-           ORDER BY 
-              f.nombre;`;
+                        f.nombre,
+                        COALESCE((
+                              SELECT SUM(pagos.valor)
+                              FROM pagos
+                              WHERE pagos.id_asignacion = a.id_asignacion
+                              AND YEAR(pagos.fecha) = ?
+                          ), 0) AS total_pagos,
+                        SUM(j.kilos) AS total_kilos
+                    FROM 
+                        fincas f
+                    JOIN 
+                        usuarios u ON f.id_propietario = u.no_identificacion
+                    JOIN 
+                        semanas s ON f.id_finca = s.id_finca
+                    JOIN 
+                        asignaciones a ON s.id_semana = a.id_semana
+                    JOIN 
+                        jornales j ON a.id_asignacion = j.id_asignacion
+                    JOIN 
+                        pagos p ON a.id_asignacion = p.id_asignacion
+                    WHERE 
+                        u.no_identificacion = ?
+                    AND 
+                       YEAR(p.fecha) = ?
+                    GROUP BY 
+                       f.nombre
+                    ORDER BY 
+                       f.nombre;`;
       return new Promise((resolve, reject) => {
-            conn.query(sql, [no_identificacion, year], (err, result) => {
+            conn.query(sql, [year, no_identificacion, year], (err, result) => {
                   if (err) return reject(err);
                   resolve(result);
             });
