f449da61ea63307df3c8902c9e8978ecf9fd0dfc
SibelJr11 <107586362+SibelJr11@users.noreply.github.com>
2025-03-02 10:15:14 -0500

    Eliminando node_modules del repositorio

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const server = http.createServer(app); // 🔹 Crear servidor HTTP
 
 const io = new Server(server, {
       cors: {
-            origin: "http://localhost:5173",
+            origin: "https://gestcafe.netlify.app",
             methods: ["GET", "POST"],
       },
 });
