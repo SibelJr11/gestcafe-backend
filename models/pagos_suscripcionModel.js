462c6a3b8bda59481a722e49982cc60fc5135667
SibelJr11 <107586362+SibelJr11@users.noreply.github.com>
2025-03-02 01:16:48 -0500

    Eliminando node_modules del repositorio

--- models/pagos_suscripcionModel.js
+++ models/pagos_suscripcionModel.js
@@ -1,15 +1,15 @@
 exports.create = (conn, pago_suscripcion) => {
-    const sql = "INSERT INTO pagos_suscripcion SET ?";
-    return new Promise((resolve, reject) => {
-          conn.query(sql, pago_suscripcion, (err, result) => {
-                if (err) return reject(err);
-                resolve(result);
-          });
-    });
+      const sql = "INSERT INTO pagos_suscripcion SET ?";
+      return new Promise((resolve, reject) => {
+            conn.query(sql, pago_suscripcion, (err, result) => {
+                  if (err) return reject(err);
+                  resolve(result);
+            });
+      });
 };
 
 exports.getHistorial = (conn, idSuscripcion) => {
-      const sql = `SELECT * FROM pagos_suscripcion WHERE id_suscripcion = ?`;
+      const sql = `SELECT * FROM pagos_suscripcion WHERE id_suscripcion = ? ORDER BY fecha_pago DESC`;
 
       return new Promise((resolve, reject) => {
             conn.query(sql, [idSuscripcion], (err, results) => {
